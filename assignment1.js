295e129b06eebccc21a89b9370242bfd54143611
Jagadeesh9211 <jagadeeshkathirvelu52@gmail.com>
2025-08-20 12:03:07 +0530

    Add files via upload
    Day 1

---
+++ assignment1.js
@@ -0,0 +1,27 @@
+var city="Kolkata";
+console.log("I live in "+ city);
+console.log(typeof city);
+
+
+const marks=85;
+console.log(marks +10);
+console.log(typeof marks);
+
+
+let isWeekend=true;
+if(isWeekend){
+ console.log("Relax");
+}
+else{
+console.log("Work");
+}
+console.log(typeof isWeekend);
+
+
+
+
+let futureGoal;
+console.log(typeof futureGoal);
+
+
+
